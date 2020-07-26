a48ebd9ae552dea3f44709d4eac038f6e07cb112
Xander-C <1019104544@qq.com>
2020-07-26 12:17:56 +0800

    refactor: change directory structure.

--- login/js/index.js
+++ login/js/index.js
@@ -1,8 +1,67 @@
 $(document).ready(function() {
     $('select').material_select();
 });
-var passwordDom = document.querySelector("#password");
-var passwordLabel = passwordDom.nextElementSibling;
+const passwordDom = document.querySelector("#password");
+const passwordLabel = passwordDom.nextElementSibling;
 //passwordDom.setAttribute("class", "invalid");
 //passwordLabel.dataset.error = "用户不存在";
-console.log(backendUrl);
+const userNameDom = document.querySelector("#userName");
+const userNameLabel = userNameDom.nextElementSibling;
+const clubDom = $("#club option:selected");
+const submit = () => {
+    
+    axios.post(getUrl(`/login/check`), {
+        club: clubDom.text(),
+        name: userNameDom.value,
+        password: passwordDom.value
+      })
+        .then(function (response) {
+            if (response.status != 200) {
+                alert("服务器出错");
+                return;
+            }
+            const data = response.json();
+            if (data.result == 0) {
+                switch (data.sucMessage) {
+                    case 1:
+                        window.location = '/member.html';
+                        break;
+                    case 2:
+                        window.location = '/department.html';
+                        break;
+                    case 3:
+                        window.location = '/club.html';
+                        break;
+                    case 4:
+                        window.location = '/superAdmin.html';
+                        break;
+                    default:
+                        alert("后端返回数据出错");
+                        break;
+                }
+            } else {
+                switch (data.errorMessage) {
+                    case 0:
+                        passwordDom.classList.remove("invalid");
+                        userNameDom.setAttribute("class", "invalid");
+                        userNameLabel.dataset.error = "用户不存在";
+                        break;
+                    case 1:
+                        userNameDom.classList.remove("invalid");
+                        passwordDom.setAttribute("class", "invalid");
+                        passwordLabel.dataset.error = "密码错误";
+                    default:
+                        alert("后端返回数据出错");
+                        break;
+                }
+            }
+      })
+        .catch(function (error) {
+            alert("服务器连接失败！");
+        console.log(error);
+      });
+}
+
+document.querySelector("#submit-btn").addEventListener("click", submit);
+
+
