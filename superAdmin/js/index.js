6678f2be6b855c641121edc1ed861667a4a436b0
XanderC <1019104544@qq.com>
2020-08-02 15:12:22 +0800

    feat: add cards for mobile page.

--- superAdmin/js/index.js
+++ superAdmin/js/index.js
@@ -1,6 +1,6 @@
 Vue.component('club-item', {
     props: ['name', 'logo', 'intro', 'username'],
-    template: '<tr class="row"><td class="col l2">{{ name }}</td><td class="col l2">{{ username }}</td><td class="col l1"><img :src=logo style="height: 18px;"></td><td class="col l5">{{ intro }}</td><td class="col l2"><a>重置密码</a></td></tr>'
+    template: '<tr class="row"><td class="col l2 m2">{{ name }}</td><td class="col l2 m2">{{ username }}</td><td class="col l1 m1"><img :src=logo style="height: 18px;"></td><td class="col l5 m4">{{ intro }}</td><td class="col l2 m3"><a class="reset-pwd">重置密码</a><a>删除社团</a></td></tr>'
 });
 let data;
 axios.get(getUrl(`/community/list`))
@@ -22,114 +22,103 @@ axios.get(getUrl(`/community/list`))
         console.log(error);
     })
 
-    
+
 $(document).ready(() => {
     $('.modal').modal();
 });
-const usernameDom = document.querySelector("#username");
+const usernameDom = document.querySelector("#add-input");
 const usernameLabel = usernameDom.nextElementSibling;
-const clubIdDom = document.querySelector("#club-id");
+const clubIdDom = document.querySelector("#undo-input");
 const clubIdLabel = clubIdDom.nextElementSibling;
-const checkUsername = () => {
-    usernameDom.classList.remove("invalid");
-    if (usernameDom.value == 0) {
-        usernameLabel.dataset.error = "用户名不能为空";
-        usernameDom.classList.add("invalid");
-        usernameLabel.classList.add("active");
+const check = (dom) => {
+    let label = dom.nextElementSibling;
+    dom.classList.remove("invalid");
+    if (dom.value == 0) {
+        label.dataset.error = `${label.innerHTML}不能为空`;
+        dom.classList.add("invalid");
+        label.classList.add("active");
         return false;
     }
     return true;
 }
-const checkClubId = () => {
-    clubIdDom.classList.remove("invalid");
-    if (clubIdDom.value == 0) {
-        clubIdLabel.dataset.error = "社团id不能为空";
-        clubIdDom.classList.add("invalid");
-        clubIdLabel.classList.add("active");
-        return false;
-    }
-    return true;
-}
-clubIdDom.addEventListener("click", () => {
-    clubIdDom.classList.remove("invalid");
+document.querySelectorAll("input").forEach(element => {
+    element.addEventListener("click", () => {
+        element.classList.remove("invalid");
+    })
 });
-usernameDom.addEventListener("click", () => {
-    usernameDom.classList.remove("invalid");
-})
+const unknowError = () => {
+    $('#confirm-title').html("未知错误");
+    $('#confirm-content').html(`请联系管理员`);
+    $('#confirm').modal('open');
+}
 
 const addClub = () => {
-    if (!checkUsername()) return;
+    let dom = document.querySelector("#add-input");
+    if (!check(dom)) return;
     $('#loading').modal('open');
     axios.post(getUrl(`/community/add`), {
-            username: usernameDom.value,
+            username: dom.value,
             password: "12345678"
         })
         .then((response) => {
+            $('#loading').modal('close');
             console.log(response);
             if (response.status != 200) {
                 window.location = "/login.html";
                 return;
             }
             if (response.data) {
-                $('#loading').modal('close');
                 $('#confirm-title').html("添加成功");
-                $('#confirm-content').html(`已添加用户${usernameDom.value}，默认登录密码为：12345678`);
+                $('#confirm-content').html(`已添加用户${dom.value}，默认登录密码为：12345678`);
                 $('#confirm').modal('open');
-                usernameDom.value = null;
-                usernameLabel.classList.remove("active");
+                dom.value = null;
+                dom.nextElementSibling.classList.remove("active");
             } else {
-                $('#loading').modal('close');
-                $('#confirm-title').html("未知错误");
-                $('#confirm-content').html(`请联系管理员`);
-                $('#confirm').modal('open');
+                console.log(response);
+                unknowError();
             }
         })
         .catch((error) => {
-            console.log(error);
             $('#loading').modal('close');
-            $('#confirm-title').html("未知错误");
-            $('#confirm-content').html(`请联系管理员`);
-            $('#confirm').modal('open');
+            console.log(error);
+            unknowError();
         })
 }
 
 const undoClub = () => {
-    if (!checkClubId()) return;
+    let dom = document.querySelector("#undo-input");
+    if (!check(dom)) return;
     $('#loading').modal('open');
     axios.post(getUrl(`/community/undo`), {
-            id: clubIdDom.value
+            id: dom.value
         })
         .then((response) => {
+            $('#loading').modal('close');
             console.log(response);
             if (response.status != 200) {
                 window.location = "/login.html";
                 return;
             }
             if (response.data) {
-                $('#loading').modal('close');
                 $('#confirm-title').html("找回成功");
-                $('#confirm-content').html(`已找回社团${clubIdDom.value}`);
+                $('#confirm-content').html(`已找回社团${dom.value}`);
                 $('#confirm').modal('open');
-                clubIdDom.value = null;
-                clubIdLabel.classList.remove("active");
+                dom.value = null;
+                dom.nextElementSibling.classList.remove("active");
             } else {
-                $('#loading').modal('close');
-                $('#confirm-title').html("未知错误");
-                $('#confirm-content').html(`请联系管理员`);
-                $('#confirm').modal('open');
+                console.log(response);
+                unknowError();
             }
         })
         .catch((error) => {
-            console.log(error);
             $('#loading').modal('close');
-            $('#confirm-title').html("未知错误");
-            $('#confirm-content').html(`请联系管理员`);
-            $('#confirm').modal('open');
+            console.log(error);
+            unknowError();
         })
 }
-document.querySelector("#add-club-btn").addEventListener("click", () => {
+document.querySelector("#add-btn").addEventListener("click", () => {
     addClub();
 });
-document.querySelector("#undo-club-btn").addEventListener("click", () => {
+document.querySelector("#undo-btn").addEventListener("click", () => {
     undoClub();
 });
